ad19ad9d8a82e191f9b62f57f60022572df5fc07
srbhtrvpt <sourabhtiruvaipati@gmail.com>
2020-06-22 14:58:54 +0200

    changed plots

--- src/plot.py
+++ src/plot.py
@@ -10,14 +10,20 @@ normalized_curvature_df=(curvature_data-curvature_data.min())/(curvature_data.ma
 
 
 fig1 = plt.figure(1)
-plt.scatter(normalized_intensity_df['values'], normalized_curvature_df['values'])
-fig1.savefig(path + "int_vs_curv.png", dpi=fig1.dpi)
+plt.scatter(intensity_data['values'], curvature_data['values'], s= 0.01**2)
+fig1.savefig(path + "int_vs_curv.png", dpi=400)
 fig2 = plt.figure(2)
 plt.boxplot(intensity_data['values'], showmeans=True, meanline=True)
 fig2.savefig(path + "intensity_boxplot.png", dpi=fig2.dpi)
 fig3 = plt.figure(3)
 plt.boxplot(curvature_data['values'], showmeans=True, meanline=True)
 fig3.savefig(path + "curvature_boxplot.png", dpi=fig3.dpi)
+fig4 = plt.figure(4)
+plt.boxplot(normalized_intensity_df['values'], showmeans=True, meanline=True)
+fig4.savefig(path + "norm_intensity_boxplot.png", dpi=fig4.dpi)
+fig5 = plt.figure(5)
+plt.boxplot(normalized_curvature_df['values'], showmeans=True, meanline=True)
+fig5.savefig(path + "norm_curvature_boxplot.png", dpi=fig5.dpi)
 
 
 
